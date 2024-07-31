0f2721fa262b58ae15a8adfd995a8b37ae59b23a
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-07-30 17:57:27 -0700

    parking lot class

--- main.py
+++ main.py
@@ -39,3 +39,23 @@ class SemiTruck(Vehicle):
     def __init__(self):
         super().__init__(3)
 
+class ParkingFloor:
+    def __init__(self, spot_count):
+        self.spots = [0] * spot_count
+        self.vehicle_map = {}
+
+    def park_vehicle(self, vehicle):
+        size = vehicle.get_spot_size()
+
+        l,r = 0,0
+        while r < len(self.spots):
+            if self.spots[r] != 0:
+                l = r +1
+            if r - 1 + 1 == size:
+                # We found enough spots, park the vehicle
+                for k in range(l, r + 1):
+                    self.spots[k] = 1
+                self.vehicle_map[vehicle] = [l,r]
+                return True
+            r += 1
+        return False
